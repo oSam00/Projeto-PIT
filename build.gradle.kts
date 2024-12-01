a76cd7a58dda503484ba47ff53009c6c0a8b58e0
oSam00 <shanderam2001@gmail.com>
2024-12-01 20:49:01 -0300

    Add files via upload

---
+++ build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+    id("com.android.application") version "8.7.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.8.0" apply false
+    id("com.google.gms.google-services") version "4.4.2" apply false
+}
+
+buildscript {
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.7.2")
+        classpath("com.google.gms:google-services:4.4.2")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.0")
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.7.2")
+        classpath("com.google.gms:google-services:4.4.2")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.0")
+    }
+}
+
+
